6e333b787c691c31519f837a03fd38637ef50d21
Sudhakaran_roni <32321500+Sudhakaran7@users.noreply.github.com>
2020-07-12 17:04:09 +0530

    Create pizza_count.py

---
+++ pizza_count.py
@@ -0,0 +1,20 @@
+tomato_slices, cheese_slices=map(int,input().split())
+Big_count=0
+small_count=0
+val=0
+if(tomato_slices>cheese_slices):
+  val=tomato_slices
+else:
+  val=cheese_slices
+for i in range(1,val):
+  if(4*i<=tomato_slices and 1*i<=cheese_slices):
+    Big_count+=1
+  else:
+    break
+for j in range(0,val):
+  if(2*j<=tomato_slices and 1*j<=cheese_slices):
+    small_count+=1
+  else:
+    break
+print('Big Pizza:',Big_count)
+print('Small Pizza:',small_count)
